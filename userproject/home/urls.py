5aee996c35c79bc70b4a4883bfe84c944511eb00
Sudip Das <sudip5428@gmail.com>
2022-05-22 21:35:16 +0530

    code commit for basic auth and model create

---
+++ userproject/home/urls.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [ 
+    path('',views.index, name="home"),
+    path('login',views.loginUser, name="login"),
+    path('logout',views.logoutUser, name="logout"),
+]
